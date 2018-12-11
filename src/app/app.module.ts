7435f45280a084d26c3d819d3b34691f57aa01c4
Pabitro Singh <pabitrosingh@gmail.com>
2018-12-11 16:54:05 +0530

    Header component , footter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
-import HeaderComponent from './shared/layout/header.component';
-import FooterComponent from './shared/layout/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/layout/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'layout-header',
-  templateUrl: './header.component.html'
-})
-export default class HeaderComponent {
-
- }
