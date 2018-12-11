56609bb9904500f2041e90e39996827178f0404f
Pabitro Singh <pabitrosingh@gmail.com>
2018-12-11 16:55:47 +0530

    ok

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  today: number = Date.now();
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
