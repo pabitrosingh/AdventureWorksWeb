c50cb098aae5bb89f830a7db7232befcb1a21dfd
Pabitro Singh <pabitrosingh@gmail.com>
2018-12-17 16:55:59 +0530

    left-side  added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { LeftsideComponent } from './leftside/leftside.component';
 
 
 
@@ -13,7 +14,8 @@ import { FooterComponent } from './shared/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LeftsideComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/leftside/leftside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leftside',
+  templateUrl: './leftside.component.html',
+  styleUrls: ['./leftside.component.css']
+})
+export class LeftsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
