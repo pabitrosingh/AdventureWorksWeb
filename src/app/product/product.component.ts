2810cbba9e5c38d5468ec0f39618acb679306d68
pabitrosingh <pabitrosingh@gmail.com>
2019-05-03 18:32:32 +0530

    added Filters Products

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _Router: Router) { }
 
   ngOnInit() {
+    
   }
 
 }
--- src/app/shared/leftside/leftside.component.ts
+++ src/app/shared/leftside/leftside.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-leftside',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftsideComponent implements OnInit {
 
-  constructor() { }
+  constructor(public Router: Router) { }
   ngOnInit() {
   }
 
