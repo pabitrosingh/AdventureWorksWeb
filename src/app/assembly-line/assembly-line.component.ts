b118e63ddfaa9478af8513f77f5e6dfbb0b3dcec
pabitrosingh <pabitrosingh@gmail.com>
2019-06-25 10:40:56 +0530

    completed Assembly Line

--- src/app/assembly-line/assembly-line.component.ts
+++ src/app/assembly-line/assembly-line.component.ts
@@ -1,48 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit  } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DBrepositoryService } from '../services/dbrepository.service';
 import { environment } from '../../environments/environment';
+import { jqxGridComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxgrid';
 
 @Component({
   selector: 'app-assembly-line',
   templateUrl: './assembly-line.component.html',
   styleUrls: ['./assembly-line.component.css']
 })
-export class AssemblyLineComponent implements OnInit {
+export class AssemblyLineComponent implements OnInit , AfterViewInit {
   LocationID: string;
   AssemblyName: string;
   AssemblyLineGridColumns: any[] = [];
   AssemblyLineGridDataAdapter: any;
   private readonly BASE_URL = environment.API_ENDPOINT;
+  @ViewChild('IDAssemblyLineGrid') IDAssemblyLineGrid: jqxGridComponent;
+  AssemblyLineGridSource: jqwidgets.GridSource;
+  AssemblyLineGridOptions: jqwidgets.GridOptions;
 
   constructor(private _ActivatedRoute: ActivatedRoute, private DB: DBrepositoryService) { }
+
   ngOnInit() {
-    // this._ActivatedRoute.params.subscribe(p => { console.log(p); } );
-    // this._ActivatedRoute.paramMap.subscribe(p => {
-    //   this.LocationID = p.get('LocationID');
-    //   console.log(this.LocationID);
-    //  });
     this.LocationID = this._ActivatedRoute.snapshot.params.LocationID;
     this.AssemblyName = this._ActivatedRoute.snapshot.params.AssemblyName;
-    this.CreateAssemblyLineGrid();
   }
 
-  CreateAssemblyLineGrid(): void {
-    this.AssemblyLineGridColumns =
-      [
-        { text: 'Work Order ID', dataField: 'WorkOrderID', width: 100, cellsalign: 'center' },
-        { text: 'Product Name', dataField: 'ProductName', width: 200 },
-        { text: 'Order Qty', dataField: 'OrderQty', width: 85, cellsalign: 'center' },
-        { text: 'Current Stage', dataField: 'CurrentStage', width: 120 },
-        { text: 'Assembly Area', dataField: 'AssemblyArea', width: 120 },
-        { text: 'Resource Hrs', dataField: 'ActualResourceHrs', width: 110, cellsalign: 'center' },
-        { text: 'Actual Cost', dataField: 'ActualCost', width: 100, cellsalign: 'center' },
-        { text: 'Start Date', dataField: 'StartDate', width: 100, cellsalign: 'center' },
-        { text: 'End Date', dataField: 'EndDate', width: 100, cellsalign: 'center' }
-      ];
+  ngAfterViewInit() {
 
-    const GridConfig: any = {
+    this.AssemblyLineGridSource =  {
       datatype: 'json',
+      url: this.BASE_URL + 'Production/GetAssemblyLineRecords/' + this.LocationID,
+      sortcolumn: 'WorkOrderID',
+      sortdirection: 'asc',
+      id: 'WorkOrderID',
+      pagesize: 5,
+      // root: 'root',
       datafields: [
         { name: 'WorkOrderID', type: 'int' },
         { name: 'ProductName', type: 'string' },
@@ -52,28 +45,44 @@ export class AssemblyLineComponent implements OnInit {
         { name: 'ActualResourceHrs', type: 'float' },
         { name: 'ActualCost', type: 'float' },
         { name: 'StartDate', type: 'string' },
-        { name: 'EndDate', type: 'string' },
+        { name: 'EndDate', type: 'string' }
       ],
-      sortcolumn: 'WorkOrderID',
-      sortdirection: 'asc',
-      id: 'WorkOrderID',
-      pagesize: 5,
-      totalrecords: 'totalrecords',
-      root: 'root',
-      cache: false,
-      url: this.BASE_URL + 'Production/GetAssemblyLineRecords/' + this.LocationID,
-      beforeprocessing: function (data) {
-        GridConfig.totalrecords  = data.totalrecords ;
-      }
+      sort: () => this.IDAssemblyLineGrid.updatebounddata('sort'),
+      filter: () => this.IDAssemblyLineGrid.updatebounddata('filter'),
     };
 
-    this.AssemblyLineGridDataAdapter = new jqx.dataAdapter(GridConfig, {
-      loadError: function (xhr, status, error) {
-      console.log(error);
-      }
-  });
+    this.AssemblyLineGridOptions  = {
+      width: 1050,
+      theme: 'office',
+      pageable: true,
+      sortable: true,
+      filterable: true,
+      autoheight: true,
+      virtualmode: true,
+      enabletooltips: true,
+      rendergridrows: (obj) =>  obj.data,
+      source: new jqx.dataAdapter(this.AssemblyLineGridSource,
+        {
+          beforeSend: function (jqxhr, settings) {
+
+         },
+          loadError: function (xhr, status, error) {
+            console.log(error);
+          }
+        }),
+      columns: [
+          { text: 'Work Order ID', datafield: 'WorkOrderID', width: 100, cellsalign: 'center' },
+          { text: 'Product Name', datafield: 'ProductName', width: 200 },
+          { text: 'Order Qty', datafield: 'OrderQty', width: 85, cellsalign: 'center' },
+          { text: 'Current Stage', datafield: 'CurrentStage', width: 120 },
+          { text: 'Assembly Area', datafield: 'AssemblyArea', width: 120 },
+          { text: 'Resource Hrs', datafield: 'ActualResourceHrs', width: 110, cellsalign: 'center' },
+          { text: 'Actual Cost', datafield: 'ActualCost', width: 100, cellsalign: 'center' },
+          { text: 'Start Date', datafield: 'StartDate', width: 100, cellsalign: 'center' },
+          { text: 'End Date', datafield: 'EndDate', width: 100, cellsalign: 'center' }
+      ]
+    };
+    this.IDAssemblyLineGrid.createComponent(this.AssemblyLineGridOptions);
   }
-  AssemblyLineGridRenderGridRows(obj) {
-    return obj.data;
-  }
+
 }
