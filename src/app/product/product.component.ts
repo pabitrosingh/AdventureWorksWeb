f8941ea19ed928d2043d3621e40f4c56ae64d019
pabitrosingh <pabitrosingh@gmail.com>
2019-07-23 12:23:24 +0530

    error-1

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -5,6 +5,7 @@ import { IProductDetails } from '../viewmodel/IProductDetails';
 import { environment } from 'src/environments/environment';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { ProductFilterService } from '../services/ProductFilter.service';
+import { IProductFilter } from '../viewmodel/IProductFilter';
 
 @Component({
   selector: 'app-product',
@@ -14,27 +15,42 @@ import { ProductFilterService } from '../services/ProductFilter.service';
 export class ProductComponent implements OnInit {
   ProductDetailsList: IProductDetails[];
   SingleProduct: IProductDetails;
-  currentRate: number = 4;
+  currentRate = 4;
   BASE_URL = environment.API_ENDPOINT;
-  @ViewChild('IDProductDetailsModelRef', { static: false}) ModalRef: ElementRef;
-  constructor(private _Router: Router ,
-              private DB: DBrepositoryService,
-              private modalService: NgbModal,
-              private _ProductFilterService: ProductFilterService) { }
+  IDTxtSearch: string;
+  @ViewChild('IDProductDetailsModelRef', { static: false }) ModalRef: ElementRef;
+
+  constructor(private _Router: Router,
+    private DB: DBrepositoryService,
+    private modalService: NgbModal,
+    private _ProductFilterService: ProductFilterService) { }
 
   ngOnInit() {
-   this._ProductFilterService.GetFilterValue().subscribe(v => {
-      if ( v ) {
-        alert(v);
-      }
-   });
+    this._ProductFilterService.GetFilterValue().subscribe(filterData => {
+      console.log(filterData);
+        this.GetProductDetails(filterData);
+    });
+  }
 
+  public BtnViewDetails(ProductID: number): void {
+    this.SingleProduct = this.ProductDetailsList.find(item => {
+      return item.ProductID === ProductID;
+    });
+    this.modalService.open(this.ModalRef, { size: 'lg', backdrop: 'static' });
+  }
+  public BtnAdtoCart(): void {
 
-    let DataToPost  = new FormData();
-    DataToPost.append('NameProductNumber', ' ');
-    DataToPost.append('ProductLine', ' ');
-    DataToPost.append('Style', ' ');
-    DataToPost.append('Class', ' ');
+  }
+
+  private GetProductDetails(filterData: IProductFilter): void {
+
+    const DataToPost = new FormData();
+    DataToPost.append('NameProductNumber', this.IDTxtSearch);
+    DataToPost.append('ProductLine', filterData.ProductLine);
+    DataToPost.append('Style', filterData.Style);
+    DataToPost.append('Class', filterData.Class);
+    DataToPost.append('Color', filterData.Color);
+    DataToPost.append('Category', filterData.Category);
 
     this.DB.GetProductDetailsFromServer(DataToPost)
       .subscribe(resp => {
@@ -45,15 +61,5 @@ export class ProductComponent implements OnInit {
         error => {
           console.log(error);
         });
-   }
-
-   public BtnViewDetails(ProductID: number): void {
-    this.SingleProduct = this.ProductDetailsList.find(item => {
-     return item.ProductID === ProductID;
-     });
-     this.modalService.open(this.ModalRef, { size: 'lg', backdrop: 'static' });
-   }
-   public BtnAdtoCart(): void {
-
-   }
+  }
 }
